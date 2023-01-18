562c40e7f991f7d61e625767fb30f3e8e5f14f47
Gaurav Jalkote <gauravjalkote012@gmail.com>
2023-01-18 14:48:48 +0530

    Login auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,13 @@ const express=require('express')
 const router=express.Router();
 //pull express validator
 const {check,validationResult}=require('express-validator');
-
+const config=require('config')
+const gravatar=require('gravatar');
+const bcrypt=require('bcryptjs');
+//Import json web token
+const jwt=require('jsonwebtoken');
+//import User schema
+const User=require('../../models/User');
 //GET api/users
 //Register user
 //public route
@@ -14,7 +20,7 @@ router.post('/',[
     check('email','Please include a valid email').isEmail(),
     //check if password field in request.body is at least 6 characters long
     check('password','Password should be at least 6 characters long').isLength({min:6})
-],(req,res)=>{
+],async (req,res)=>{
     //check the errors in req.body
     const errors=validationResult(req);
     //Check errors are not empty
@@ -23,6 +29,66 @@ router.post('/',[
         errors:errors.array()
     });
 
+    //destructing req.body 
+    const {name,email,password}=req.body;
+    
+    try{
+        //find user if same email already exists
+        let user=await User.findOne({email});
+        if(user)
+        {
+            res.status(400).json({
+                errors:[{
+                    msg:'User already exists'
+                }]
+            });
+        }
+    }catch(error)
+    {
+        //display error message
+        console.log(err.message);
+        // set status 500 Internal server error
+        res.status(500).send('Server error');
+    }
+    //see if user exists
+    //Get users gravatar
+    const avatar=gravatar.url(email,{
+        //size
+        s:'200',
+        //rating
+        r:'pg',
+        //default
+        d:'mm'
+    }
+
+    )
+
+    user=new User({
+        name,
+        email,
+        avatar,
+        password
+    });
+
+    const salt=await bcrypt.genSalt(10);
+    user.password=await bcrypt.hash(password,salt);
+    await user.save();
+    const payload={
+        user:{
+            id:user.id
+        }
+    }
+
+    jwt.sign(payload,
+        config.get('jwtSecret'),
+        {expiresIn:36000},
+        (err,token)=>{
+            if(err) throw err;
+            res.json({token});
+        });
+    res.send({token});
+    //Encrypt gravatar
+    //Return jsonwebtoken
     res.send('User route');
     //Display request body
     console.log(req.body);
