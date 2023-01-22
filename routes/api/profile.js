c5909f71e6be4bfb4cb1b424c972848ed8d2b003
Gaurav Jalkote <gauravjalkote012@gmail.com>
2023-01-23 00:51:40 +0530

    Profile route set

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,28 @@
 const express=require('express')
 //connect to express router
 const router=express.Router();
-
-
-//GET api/profile
-//Test route
-//public route
-router.get('/',(req,res)=>res.send('Profile route'));
+const auth=require('../../middleware/auth')
+const Profile =require('../../models/Profile')
+const User =require('../../models/User');
+//GET api/profile/me
+//Get current users profile
+//private route
+router.get('/me',auth,async(req,res)=>{
+    try{
+        //find profile of user
+        const profile=await Profile.findOne({user:req.user.id}).populate('user',['name','avatar']);
+        if(!profile){
+            return res.status(400).json({
+                //If there is no profile data then display this message
+                msg:'There is no profile data for this user'
+            })
+        }
+        res.json(profile);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+    res.send('Profile route');
+})
 //export route
 module.exports=router;
